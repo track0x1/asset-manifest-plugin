60eb57a703abcb0fa8eb560e24556f3b65337bec
Tom Beute <beute.thomas@gmail.com>
2018-06-25 18:12:44 -0400

    Asset manifest plugin for Webpack 4+

---
+++ index.js
@@ -0,0 +1,53 @@
+const defaultOptions = {
+  includeMap: false,
+  includePublicPath: true,
+  ignoreEmpty: true,
+  fileName: 'manifest.json',
+}
+
+class ManifestPlugin {
+  constructor(opts) {
+    this.options = Object.assign({}, defaultOptions, opts);
+  }
+
+  apply(compiler) {
+    compiler.hooks.emit.tapAsync('ManifestPlugin', (compilation, callback) => {
+      const manifest = {};
+      const basePath = this.options.includePublicPath ?
+        compilation.mainTemplate.getPublicPath({hash: compilation.hash}) : '';
+
+      compilation.chunks.forEach((chunk) => {
+        chunk.files.forEach((filename) => {
+          if (!chunk.name && this.options.ignoreEmpty) return;
+          let ref = manifest[chunk.name];
+          if (ref === undefined) {
+            ref = {};
+            manifest[chunk.name] = ref;
+          }
+
+          if (filename.endsWith('css')) {
+            ref.css = filename;
+          } else if (filename.endsWith('css.map') && this.options.includeMap) {
+            ref.cssMap = filename;
+          } else if (filename.endsWith('js')) {
+            ref.js = filename;
+          } else if (filename.endsWith('js.map') && this.options.includeMap) {
+            ref.jsMap = filename;
+          }
+        });
+      });
+
+      // Insert this list into the webpack build as a new file asset:
+      const json = JSON.stringify(manifest, null, 2)
+      compilation.assets[this.options.fileName] = {
+        source: () => json,
+        size: () => json.length
+      };
+
+      callback();
+    });
+  }
+}
+
+module.exports = ManifestPlugin;
+
